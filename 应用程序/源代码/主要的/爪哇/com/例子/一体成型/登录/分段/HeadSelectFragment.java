caf251e34df451959953427e3613ba0332a3d856
sennnna <107630584+sennnna@users.noreply.github.com>
2022-10-24 11:38:35 +0800

    Update and rename app/src/main/java/com/example/unibody/login/fragment/HeadSelectFragment.java to 应用程序/源代码/主要的/爪哇/com/例子/一体成型/登录/分段/HeadSelectFragment.java
    Add a dependency and modify a running error

--- 应用程序/源代码/主要的/爪哇/com/例子/一体成型/登录/分段/HeadSelectFragment.java
+++ 应用程序/源代码/主要的/爪哇/com/例子/一体成型/登录/分段/HeadSelectFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import androidx.lifecycle.Observer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
