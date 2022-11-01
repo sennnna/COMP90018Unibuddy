afc3107c9316551a6c2390a363bbc513b5a02eea
sennnna <107630584+sennnna@users.noreply.github.com>
2022-11-02 00:42:45 +0800

    Update QuestionFragment.java

--- app/src/main/java/com/example/unibody/login/fragment/QuestionFragment.java
+++ app/src/main/java/com/example/unibody/login/fragment/QuestionFragment.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioGroup;
+import android.widget.EditText;
 
 import com.example.unibody.R;
 
