9b9b0342fcd2473afe6a51a8cf915fcdf6078335
sennnna <107630584+sennnna@users.noreply.github.com>
2022-11-02 00:16:18 +0800

    Update ChatFragment.java

--- app/src/main/java/com/example/unibody/chat/fragment/ChatFragment.java
+++ app/src/main/java/com/example/unibody/chat/fragment/ChatFragment.java
@@ -1,66 +1,76 @@
 package com.example.unibody.chat.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.unibody.R;
+import com.example.unibody.chat.activity.ChatDetailsActivity;
+import com.example.unibody.chat.adapter.ChatListAdapter;
+import com.example.unibody.chat.domain.Chat;
+import com.example.unibody.chat.viewmodel.ChatViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
 public class ChatFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ChatFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChatFragment newInstance(String param1, String param2) {
-        ChatFragment fragment = new ChatFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView chat_list;
+    private ChatViewModel chatViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        chatViewModel = new ViewModelProvider(requireActivity(),
+                new ViewModelProvider.AndroidViewModelFactory(requireActivity().getApplication())).get(ChatViewModel.class);
+        ChatViewModel.setChatViewModel(chatViewModel);
+        chatViewModel.chatLiveData.observe(requireActivity(), new Observer<List<Chat>>() {
+            @Override
+            public void onChanged(List<Chat> chats) {
+                ChatListAdapter chatListAdapter = new ChatListAdapter(chats);
+                chat_list.setAdapter(chatListAdapter);
+                chatListAdapter.setOnItemClickListener(new ChatListAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(int position) {
+                        Intent intent = new Intent(requireContext(), ChatDetailsActivity.class);
+                        intent.putExtra("chat_id",chats.get(position).id);
+                        intent.putExtra("index",position);
+                        startActivity(intent);
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    private void initView(View view){
+
+        requireActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+        requireActivity().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//实现状态栏图标和文字颜色为暗色
+
+        chat_list = view.findViewById(R.id.chat_list);
+
+        chat_list.setLayoutManager(new LinearLayoutManager(requireContext()));
     }
 }
